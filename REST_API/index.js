904ebe9c64b2dfc63128473779ab1f2d26b25542
Anuj Karambalkar <115656468+Anuj-K15@users.noreply.github.com>
2025-03-24 01:09:09 +0530

    delete rout in rest api

--- REST_API/index.js
+++ REST_API/index.js
@@ -69,6 +69,12 @@ app.get('/posts/:id/edit', (req, res) => {
     let post = posts.find((p) => id === p.id);
     res.render('edit.ejs', { post });
 });
+
+app.delete('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect('/posts');
+});
 app.listen(port, () => {
     console.log('Port is listening on 8080');
 });
